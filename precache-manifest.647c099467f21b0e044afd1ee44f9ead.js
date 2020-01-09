99b911ff6ef61c72bbfc9a0f911d20bb69fec050
oleg131 <oleg131@mail.ru>
2020-01-10 03:50:12 +0900

    Updates

---
+++ precache-manifest.647c099467f21b0e044afd1ee44f9ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767e650e512f5ec2dac396aa5ad909cc",
+    "url": "/metal-wall/index.html"
+  },
+  {
+    "revision": "ecee03edd3a60dc97630",
+    "url": "/metal-wall/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "67f02bd291442c569a26",
+    "url": "/metal-wall/static/css/main.5e385ff3.chunk.css"
+  },
+  {
+    "revision": "ecee03edd3a60dc97630",
+    "url": "/metal-wall/static/js/2.e3611e03.chunk.js"
+  },
+  {
+    "revision": "67f02bd291442c569a26",
+    "url": "/metal-wall/static/js/main.92829f3f.chunk.js"
+  },
+  {
+    "revision": "37078f3542174705ce25",
+    "url": "/metal-wall/static/js/runtime-main.6336aa32.js"
+  }
+]);
