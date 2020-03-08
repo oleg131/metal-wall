c8fd4900c4349be7d1525902ecc47e2e57452fef
None <none@none.com>
2020-03-08 16:22:07 -0700

    lint and backface visibility in css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import moment from 'moment';
 import './App.css';
 import placeholderImage from './0.png';
 
-const CORS_HOST = process.env.REACT_APP_CORS_HOST || 'http://localhost:8080/'
+const CORS_HOST = process.env.REACT_APP_CORS_HOST || 'http://localhost:8080/';
 const THUMBNAIL_SIZE = 120;
 
 function App() {
@@ -25,7 +25,7 @@ function Header() {
         Wall of latest metal releases. Powered by Metal Archives, YouTube and React.
       </h2>
     </div>
-  )
+  );
 }
 
 function Home() {
@@ -45,82 +45,82 @@ function Home() {
     &country=&location=&releaseLabelName=&releaseCatalogNumber=&releaseIdentifiers=
     &releaseRecordingInfo=&releaseDescription=&releaseNotes=&genre=&releaseType[]=1
     &sEcho=1&iColumns=3&sColumns=&iDisplayStart=DISPLAYSTART
-    &iDisplayLength=${displayLength}`.replace(/\n/g, '').replace(/ /g, '')
+    &iDisplayLength=${displayLength}`.replace(/\n/g, '').replace(/ /g, '');
 
   const n_col = Math.floor(window.innerWidth / THUMBNAIL_SIZE);
   const n_row = Math.ceil(2 * window.innerHeight / THUMBNAIL_SIZE);
   const n_load = n_col * n_row;
 
   useEffect(() => {
-    console.log('loading init n', n_load)
+    console.log('loading init n', n_load);
     fetchInit().then(total => {
       const initItems = [];
       fetchItems(total - displayLength, initItems);
-    })
-  }, [])
+    });
+  }, []);
 
   useEffect(() => {
-    console.log('items', items.length)
-  }, [items])
+    console.log('items', items.length);
+  }, [items]);
 
   function fetchItems(displayStart, items) {
     fetchData(displayStart)
       .then(data => {
-        items.push(...data)
+        items.push(...data);
         if (items.length < n_load) {
-          fetchItems(displayStart - displayLength, items)
+          fetchItems(displayStart - displayLength, items);
         } else {
-          setItems(items)
-          setStart(displayStart - displayLength)
+          setItems(items);
+          setStart(displayStart - displayLength);
         }
-      })
+      });
   }
 
   function fetchInit() {
-    console.log('fetch initial')
+    console.log('fetch initial');
     return fetch(url.replace('DISPLAYSTART', 0))
       .then(res => res.json())
       .then(function(data) {
-        return data.iTotalRecords
-      })
+        return data.iTotalRecords;
+      });
   }
 
   function fetchData(displayStart) {
-    console.log('fetch with start', displayStart)
+    console.log('fetch with start', displayStart);
     return fetch(url.replace('DISPLAYSTART', displayStart))
       .then(res => res.json())
       .then(function(data) {
         if (data.aaData.length === 0) {
-          return []
+          return [];
         }
 
         data = data.aaData.map(processData).filter(a => !!a);
         data = data.filter(a => a.date <= moment().format("YYYY-MM-DD"));
         data.sort((a, b) => a.date < b.date).reverse();
-        console.log('fetched', data.length)
+        console.log('fetched', data.length);
 
-        return data
-      })
+        return data;
+      });
   }
 
   function fetchScroll(displayStart) {
     fetchData(displayStart)
       .then(data => {
         if (!data.length) {
-          sethasMore(false)
+          sethasMore(false);
         } else {
           setItems(items => (
             items.slice().concat(data)//.sort((a, b) => a.date < b.date).reverse()
-          ))
-          setStart(displayStart - displayLength)
+          ));
+          setStart(displayStart - displayLength);
         }
-      })
+      });
   }
 
   function processData(data) {
     const m = data[2].match('<!-- (.+) -->');
     if (!m) {
-      return null
+      return null;
     }
     const date = m[1];
 
@@ -135,11 +135,11 @@ function Home() {
     const as = el1.getElementsByTagName('a');
     const url = as[0].getAttribute('href');
     const id = url.split('/').pop();
-    const cover = `https://www.metal-archives.com/images/${id[0]}/${id[1]}/${id[2]}/${id[3]}/${id}.`
+    const cover = `https://www.metal-archives.com/images/${id[0]}/${id[1]}/${id[2]}/${id[3]}/${id}.`;
 
     return {
       url, id, cover, date, artist, album
-    }
+    };
   }
 
   return (
@@ -172,12 +172,13 @@ function Album({ data }) {
   const width = useWindowWidth();
 
   function useWindowWidth() {
-    const [width, setWidth] = useState(window.innerWidth);
-
-    // console.log(window.innerWidth, document.body.clientWidth)
+    const [width, setWidth] = useState(document.body.clientWidth);
 
     useEffect(() => {
-      const handleResize = () => setWidth(window.innerWidth);
+      // console.log(document.body.clientWidth);
+      // setWidth(document.body.clientWidth);
+      
+      const handleResize = () => setWidth(document.body.clientWidth);
       window.addEventListener('resize', handleResize);
       return () => {
         window.removeEventListener('resize', handleResize);
@@ -206,14 +207,14 @@ function Album({ data }) {
         />
       </a>
     </div>
-  )
+  );
 }
 
 function Empty({ onLoad }) {
-  useEffect(onLoad)
+  useEffect(onLoad);
   return (
     <Fragment />
-  )
+  );
 }
 
 export default App;
